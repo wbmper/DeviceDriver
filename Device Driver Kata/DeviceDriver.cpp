a98b5179738d5d84eda765b03e9660d156f1a4b7
yuz010 <rladbwmd123@gmail.com>
2025-03-31 11:37:24 +0900

    [Feature] Device Driver

--- Device Driver Kata/DeviceDriver.cpp
+++ Device Driver Kata/DeviceDriver.cpp
@@ -1,4 +1,6 @@
 #include "DeviceDriver.h"
+#include <unordered_map>
+#include <vector>
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {
@@ -6,12 +8,27 @@ DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    std::vector<unsigned char> results;
+
+    for (int readTimes = 0; readTimes < 5; readTimes++) {
+        results.push_back(m_hardware->read(address));
+    }
+
+    unsigned char value = results[0];
+    for (int readTimes = 1; readTimes < 5; readTimes++) {
+        if (value != results[readTimes]) {
+            throw ReadFailException();
+        }
+    }
+    return value;
 }
 
 void DeviceDriver::write(long address, int data)
 {
-    // TODO: implement this method
-    m_hardware->write(address, (unsigned char)data);
+    if (m_hardware->read(address) == 0xFF) {
+        m_hardware->write(address, (unsigned char)data);
+        return;
+    }
+
+    throw WriteFailException();
 }
